b15b9b04753dd80811dc255ef15d3b7ad6d6b11a
Vahid SAMAVATIAN <v.samavatian@gmail.com>
2023-08-31 12:25:09 +0330

    Added index.html, styles.css, and script.js files, CCTHELP.ctf

---
+++ webapp/script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Your JavaScript code goes here
+    // Example: document.getElementById("myButton").addEventListener("click", function() {...});
+});
