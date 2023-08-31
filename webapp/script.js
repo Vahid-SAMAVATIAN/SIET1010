c6074f50dc7db028b49b5ee1500b17c527ad43fd
Vahid SAMAVATIAN <v.samavatian@gmail.com>
2023-08-31 13:07:49 +0330

    Added index.html, styles.css, and script.js files

--- webapp/script.js
+++ webapp/script.js
@@ -1,4 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Your JavaScript code goes here
-    // Example: document.getElementById("myButton").addEventListener("click", function() {...});
+    // Function to perform the calculation
+    function performCalculation() {
+        // Load the MATLAB Component Technology File (CTF)
+        const ctf = CCTHelp.loadCTF('CCTHelp.ctf');
+
+        // Call a function from the CTF to perform the calculation
+        const result = ctf.performCalculation();
+
+        // Display the result
+        const resultElement = document.getElementById("result");
+        resultElement.textContent = "Result: " + result;
+    }
+
+    // Event listener for the button click
+    const calculateButton = document.getElementById("calculateButton");
+    calculateButton.addEventListener("click", performCalculation);
 });
